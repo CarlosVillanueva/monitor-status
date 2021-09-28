5ec28affb6cbfb7862a946e719c01c4653bae2f6
unknown <carlos.villanueva.rivera@gmail.com>
2021-09-28 14:44:58 -0300

    monitor1.6

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 // use Auth;
 
 use Illuminate\Http\Request;
-use App\Models\propiedades as propied;
+use App\Models\Propiedades as propied;
 
 
 class PageController extends Controller
--- app/Models/Propiedades.php
+++ app/Models/Propiedades.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class propiedades extends Model
+class Propiedades extends Model
 {
     use HasFactory;
 
