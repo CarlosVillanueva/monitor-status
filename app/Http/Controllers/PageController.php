9a66623b005b0c268ae0fc4b518c3844aa4800d9
unknown <carlos.villanueva.rivera@gmail.com>
2021-09-28 14:01:28 -0300

    monitor1.2

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 // use Auth;
 
 use Illuminate\Http\Request;
-use App\models\Propiedades as Propiedades;
+use App\models\Propiedades as propiedades;
 
 
 class PageController extends Controller
@@ -12,7 +12,7 @@ class PageController extends Controller
 
     public function index()
     {
-        $data = Propiedades::all();
+        $data = propiedades::all();
         // dd($data);
         return view('home')->with(['data'=>$data]);
     }
